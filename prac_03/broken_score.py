73c2b6ca97a2c94f29210cbdc7a3e1fab911931b
jc782620 <jarrod.paynewatson@my.jcu.edu.au>
2021-08-17 20:12:22 +1000

    Refactor broken score program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,15 +2,31 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 EXCELLENT_THRESHOLD = 90
 PASS_THRESHOLD = 50
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= EXCELLENT_THRESHOLD:
-    print("Excellent")
-elif score >= PASS_THRESHOLD:
-    print("Passable")
-else:
-    print("Bad")
+
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+
+# c
+    generated_score = random.randint(0, 101)
+    print(generated_score, determine_result(generated_score))
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= EXCELLENT_THRESHOLD:
+        return "Excellent"
+    elif score >= PASS_THRESHOLD:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
 
