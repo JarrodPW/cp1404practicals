2da10e843099f31b886fce5ed7723fc4ec2189c5
jc782620 <jarrod.paynewatson@my.jcu.edu.au>
2021-08-24 15:01:27 +1000

    Add 'woefully inadequate security checker' task

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -26,3 +26,12 @@ def calculate_average(numbers):
 
 
 main()
+
+# 2. Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
