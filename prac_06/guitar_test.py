8b1cca37c79b1f4946c72f0b002becf1369bf6c3
jc782620 <jarrod.paynewatson@my.jcu.edu.au>
2021-09-07 15:10:24 +1000

    Update guitar and guitars_test files

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+from prac_06.guitar import Guitar
+
+guitar_one = Guitar("Gibson L-5 CES", 1922, 16035.40)
+guitar_two = Guitar("Another Guitar", 2013)
+print(f"{guitar_one.name} get_age() - Expected 99 got {guitar_one.get_age()}")
+print(f"{guitar_two.name} get_age() - Expected 8 got {guitar_two.get_age()}")
+print(f"{guitar_one.name} is_vintage() - Expected True got {guitar_one.is_vintage()}")
+print(f"{guitar_two.name} is_vintage() - Expected False got {guitar_two.is_vintage()}")
+
+print(f"50-year old guitar is_vintage() - Expected True got {guitar_one.is_vintage()}")
+print(f"50-year old guitar is_vintage() - Expected False got {guitar_two.is_vintage()}")
