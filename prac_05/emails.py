2c3adce13851ec39d5e24775c9e4dc2bcb4b5a4f
jc782620 <jarrod.paynewatson@my.jcu.edu.au>
2021-09-07 12:00:19 +1000

    Rewrite lost code for emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,16 +2,27 @@
 Write a program that stores users' emails (unique keys) and names (values) in a dictionary.
 """
 
-email_to_name = {}
 
-email = input("Email: ")
-while email != "":
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        answer = input(f"Is your name {name}? (Y/n) ").lower()
+        if answer != "y" and answer != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    print("")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name_from_email(email):
     prefix = email.split("@")[0]
     parts = prefix.split(".")
     name = " ".join(parts).title()
-    print(f"Is your name {name}? (Y/n)", end ="")
-    choice = input("").lower()
-    if choice == "n":
-        name = input("Name: ")
-    email = input("Email: ")
-for 
+    return name
+
+
+main()
